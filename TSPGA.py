54b26c2690dde1755cea2847839bab4a5efd7e3a
Roleola Hameed (2019) ZHAC325 <hameed.roleola.2019@live.rhul.ac.uk>
2022-11-14 03:26:24 +0000

    Updated GA program.

--- TSPGA.py
+++ TSPGA.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 # For the TSP a gene in this case would be a city with its coordinates. 
-
 class City:
     def __init__(self, node, x, y):
         self.node = int(node)
 
+   def getx(self):
+      return self.x
+   
+   def gety(self):
+      return self.y
+   
     # Calculates the Euclidean distance between 2 points
     def distance(self, city):
         x_len = self.x - city.x
@@ -14,7 +19,21 @@ class City:
         return distance
 
     def to_string(self):
+        
         return "city" + str(self.node) + " x: " + str(self.x) + " y: " + str(self.y)
 
     def __repr__(self):
         return self.to_string()
+
+
+# Use class for fitness function to define inverse of route
+class FitnessFunc:
+    def __init__(self, route):
+        self.route = route
+        self.distance = 0
+        self.fitness = 0.0
+
+    def routeDistance(self):
+        if self.distance ==0:
+            routDis = 0
+            lenRoute = len(self.route)
