6ad106db050694a49ecbf760cdf3c4e30074fdce
Roleola Hameed (2019) ZHAC325 <hameed.roleola.2019@live.rhul.ac.uk>
2023-02-11 02:59:57 +0000

    Updating and reformatting the code.

--- src code/prototype1.py
+++ src code/prototype1.py
@@ -1,68 +1,72 @@
 # Import everything from tkinter and the font module
-from tkinter import *
+import tkinter
 import tkinter.messagebox
 import pygame
+from TSPGA_application import main
 
-
-# Import the TSPGA_application module
-from TSPGA_application import *
-
-class Window(Frame):
+class TSPGAGUI(tkinter.Frame):
+    """Class representing the GUI for the TSPGA simulation"""
 
     def __init__(self, master=None):
-        Frame.__init__(self, master)
+        tkinter.Frame.__init__(self, master)
         self.master = master
-        self.init_window()
-
-    # Initialize the window with widgets
-    def init_window(self):
+        self._init_window()
+    
+    def _init_window(self):
+        """Initialize the GUI window with widgets"""
         self.master.title("TSP Genetic Algorithm")
-
-        '''allowing the widget to take the full space of the root window
-        so widgit is continuously shown even when root window has been changed
-        in size'''
-
-        self.pack(fill=BOTH, expand=1)
-
-        title_label = Label(self, text="Travelling Salesman Problem", font=("Helvetica", 16))
+        self.pack(fill=tkinter.BOTH, expand=1)
+        
+        # Add a title label
+        title_label = tkinter.Label(
+            self, text="Travelling Salesman Problem", font=("Helvetica", 16)
+        )
         title_label.pack(pady=20)
 
-        # Creating a menu instance
-        menu = Menu(self.master, tearoff=False)
+        # Add a menu bar
+        menu = tkinter.Menu(self.master, tearoff=False)
         self.master.config(menu=menu)
 
         # Create the Simulator object
-        Simulator = Menu(menu)
-        menu.add_command(label="Simulator", command=self.Run)
+        simulator_menu = tkinter.Menu(menu)
+        menu.add_command(label="Simulator", command=self.run)
 
         # Create the help_centre object
-        help_centre = Menu(menu)
-        menu.add_command(label="Info", command=self.Help)
+        help_menu = tkinter.Menu(menu)
+        menu.add_command(label="Info", command=self.help)
 
         # Button to close the window
-        button_1 = Button(root, text="Force Quit", command=root.destroy)
-        button_1.pack(pady=50)
+        button = tkinter.Button(self.master, text="Force Quit", command=self.quit)
+        button.pack(pady=50)
 
     # Show information about the TSP Genetic Algorithm
-    def Help(self): 
-        root = tkinter.Tk()
-        root.geometry("400x250")
-        root.config(bg="#856ff8")
-        T = tkinter.Text(root, height=10, width=40, font=("Helvetica", 12), bg="#856ff8", fg="white")
-        T.pack()
-        T.insert(tkinter.END, "Genetic Algorithm TSP\n\nThis is an application demonstrating\nhow the travelling salesman problem\nworks. To run it, select the Simulator\noption in the menu.")
-        tkinter.mainloop()
+    def help(self):
+        """Open a window with information about TSPGA"""
+        help_window = tkinter.Tk()
+        help_window.geometry("400x250")
+        help_window.config(bg="#856ff8")
+        text = tkinter.Text(
+            help_window, height=10, width=40, font=("Helvetica", 12),
+            bg="#856ff8", fg="white")
+        text.pack()
+        text.insert(
+            tkinter.END, 
+            "Genetic Algorithm TSP\n\nThis is an application demonstrating\n"
+            "how the travelling salesman problem\nworks. To run it, select the "
+            "Simulator\noption in the menu."
+        )
+        help_window.mainloop()
 
-
-    # Run the main function from the TSPGA_application module
-    def Run(self):
+    def run(self):
+        """Run the main function from the TSPGA_application module"""
         main()
 
 ''' root window created. Here, that would be the only window, but
  you can later have windows within windows.'''
 
 # Create the root GUI window
-root = Tk()
-root.geometry("400x250")
-app = Window(root)
-root.mainloop()
+if __name__ == "__main__":
+    root = tkinter.Tk()
+    root.geometry("400x250")
+    app = TSPGAGUI(root)
+    root.mainloop()
